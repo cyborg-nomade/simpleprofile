54a34e88758e46503e0926e8ee7a5ab4f84596e3
Uriel Fiori <uriel.fiori@gmail.com>
2019-05-22 18:07:39 -0300

    changes to name, and basic user profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BoundAttribute } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myangular';
+  title = 'Hello World';
+  showAddress = true;
+  person = {
+    name: 'John Dee',
+    age: 34,
+    addres: {
+      street: '400 Walnut St',
+      city: 'Lynn',
+      state: 'MA'
+    },
+    avatar:
+      'https://zcoin.io/wp-content/uploads/2017/01/blank-avatar-300x300.png',
+    friends: [
+      { name: 'Bob', age: 34 },
+      { name: 'Shelly', age: 36 },
+      { name: 'Jack', age: 32 },
+      { name: 'Steve', age: 40 }
+    ]
+  };
+
+  constructor() {
+    this.title = 'User Profile';
+  }
 }
